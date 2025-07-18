cd0c246305e680a5b35331e14538506f41b86887
heresyrt <heresyrt@gmail.com>
2025-07-18 15:28:21 +0900

    Initial commit2

---
+++ src/components/BuField1/buField1Data.tsx
@@ -0,0 +1,35 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    image: "/images/buField/buField1-self1.jpg",
+    title: "옹벽 사면 절취",
+  },
+  {
+    id: 2,
+    image: "/images/buField/buField1-self2.jpg",
+    title: "천공 및 터파기 및 기초 작업",
+  },
+  {
+    id: 3,
+    image: "/images/buField/buField1-self3.jpg",
+    title: "H-Pile 또는 강관 파일 설치 후 콘크리트 타설",
+  },
+  {
+    id: 4,
+    image: "/images/buField/buField1-self4.jpg",
+    title: "토류판설치 및 그라우팅",
+  },
+  {
+    id: 5,
+    image: "/images/buField/buField1-self5.jpg",
+    title: "PC 패널 설치",
+  },
+  {
+    id: 6,
+    image: "/images/buField/buField1-self6.jpg",
+    title: "마감",
+  },
+];
+export default featuresData;
