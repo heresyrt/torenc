4e1f4f6a3ad1ec54d9fcb38549020379d0e144b7
heresyrt <heresyrt@gmail.com>
2025-07-25 14:40:54 +0900

    cors3

--- next.config.js
+++ next.config.js
@@ -1,36 +1,13 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
       },
-    ];
-  },
-
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: { and: [/\.(js|ts)x?$/] },
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+    ],
   },
 };
 
