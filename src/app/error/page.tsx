67f9e64a9eff74634e6d3525addb2d2c6f7d9ad1
heresyrt <heresyrt@gmail.com>
2025-07-29 14:58:59 +0900

    SEO 수정

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,9 +3,28 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Error Page | 더오름이앤씨 | 031-869-3453",
-  description: "This is Error Page for Startup Nextjs Template",
-  // other metadata
+  title: "더오름이앤씨 | 031-869-3453",
+  description: "더오름이앤씨,자립식옹벽,절토부옹벽,성토부옹벽,토목공사",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+    },
+  },
+  openGraph: {
+    title: '더오름이앤씨 | 031-869-3453',
+    description: '자립식옹벽,절토부옹벽,성토부옹벽,토목공사',
+    type: 'website',
+    url: 'https://torenc.com',
+    siteName: '더오름이앤씨',
+    locale: 'ko_KR',
+  },
+  icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 const ErrorPage = () => {
